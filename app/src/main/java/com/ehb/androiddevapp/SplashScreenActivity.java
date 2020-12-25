c252eb0864695258c3aeff88ac449f1bf5e46f52
alvi.istamalov <alvi.istamalov@student.ehb.be>
2020-12-25 21:21:01 +0100

    Added Splash Screen, Login & Register activities

---
+++ app/src/main/java/com/ehb/androiddevapp/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.ehb.androiddevapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int LIMIT= 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //een splash screen tonen (2 sec lang) en daarna door verwijzen naar de main activity
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, LIMIT);
+    }
+}
